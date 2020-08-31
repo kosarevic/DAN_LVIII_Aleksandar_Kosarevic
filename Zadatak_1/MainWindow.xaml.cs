58b851454a83de15d00dc85977c57c9aad8027f6
Kosarevic <kosarevic@gmail.com>
2020-08-31 14:30:29 +0200

    Implemented entire interactive logic for Tic Tac Toe game.

--- Zadatak_1/MainWindow.xaml.cs
+++ Zadatak_1/MainWindow.xaml.cs
@@ -20,42 +20,181 @@ namespace Zadatak_1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Button> gameButtons;
-
-        public static bool TURN = true;
+        MainWindowViewModel mvm = new MainWindowViewModel();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            gameButtons = new List<Button>();
+            mvm.GameButtons.Add(A1);
+            mvm.GameButtons.Add(A2);
+            mvm.GameButtons.Add(A3);
+            mvm.GameButtons.Add(B1);
+            mvm.GameButtons.Add(B2);
+            mvm.GameButtons.Add(B3);
+            mvm.GameButtons.Add(C1);
+            mvm.GameButtons.Add(C2);
+            mvm.GameButtons.Add(C3);
 
-            gameButtons.Add(A1);
-            gameButtons.Add(A2);
-            gameButtons.Add(A3);
-
-            gameButtons.Add(B1);
-            gameButtons.Add(B2);
-            gameButtons.Add(B3);
-
-            gameButtons.Add(C1);
-            gameButtons.Add(C2);
-            gameButtons.Add(C3);
+            DataContext = mvm;
         }
 
-        private void onRestartButton_Click(object sender, RoutedEventArgs e)
+        private void OnRestartButton_Click(object sender, RoutedEventArgs e)
         {
-
+            mvm.ResetTheGame();
         }
 
-        private void aboutButton_Click(object sender, RoutedEventArgs e)
+        private void GameAction_Click(object sender, RoutedEventArgs e)
         {
+            Button pressedButton = (Button)sender;
 
+            pressedButton.Content = mvm.X_SYMBOL;
+            pressedButton.IsEnabled = false;
+
+            if (CheckGameStatus())
+                return;
+
+            mvm.PerformAiMove();
+
+            if (CheckGameStatus())
+                return;
         }
 
-        private void gameAction_Click(object sender, RoutedEventArgs e)
+        private bool CheckGameStatus()
         {
+            if (CheckHorizontal())
+            {
+                mvm.DisableGame();
+                MessageBox.Show("Player " + mvm.Winner + " has won the game!");
+                return true;
+            }
 
+            if (CheckVertical())
+            {
+                mvm.DisableGame();
+                MessageBox.Show("Player " + mvm.Winner + " has won the game!");
+                return true;
+            }
+
+            if (CheckDiagonal())
+            {
+                mvm.DisableGame();
+                MessageBox.Show("Player " + mvm.Winner + " has won the game!");
+                return true;
+            }
+
+            if (mvm.CheckForTie())
+            {
+                mvm.ResetTheGame();
+                MessageBox.Show("The game ended in a tie!");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckHorizontal()
+        {
+            bool gameOver = false;
+            // Horizontal check
+            if (A1.Content.Equals(A2.Content)
+                && A1.Content.Equals(A3.Content)
+                && A2.Content.Equals(A3.Content)
+                && !A1.Content.Equals(""))
+            {
+                // Top row won
+                //MessageBox.Show("top row");
+                gameOver = true;
+                mvm.Winner = Convert.ToString(A1.Content);
+            }
+            else if (B1.Content.Equals(B2.Content)
+                    && B1.Content.Equals(B3.Content)
+                    && B2.Content.Equals(B3.Content)
+                    && !B1.Content.Equals(""))
+            {
+                // Middle row won
+                //MessageBox.Show("middle row");
+                gameOver = true;
+                mvm.Winner = Convert.ToString(B1.Content);
+            }
+            else if (C1.Content.Equals(C2.Content)
+                    && C1.Content.Equals(C3.Content)
+                    && C2.Content.Equals(C3.Content)
+                    && !C1.Content.Equals(""))
+            {
+                // Bottom row won
+                //MessageBox.Show("bottom row");
+                gameOver = true;
+                mvm.Winner = Convert.ToString(C1.Content);
+            }
+
+            return gameOver;
+        }
+
+        private bool CheckVertical()
+        {
+            bool gameOver = false;
+            // Vertical check
+            if (A1.Content.Equals(B1.Content)
+                && A1.Content.Equals(C1.Content)
+                && B1.Content.Equals(C1.Content)
+                && !A1.Content.Equals(""))
+            {
+                // Left column won
+                //MessageBox.Show("left column");
+                gameOver = true;
+                mvm.Winner = Convert.ToString(A1.Content);
+            }
+            else if (A2.Content.Equals(B2.Content)
+                    && A2.Content.Equals(C2.Content)
+                    && B2.Content.Equals(C2.Content)
+                    && !A2.Content.Equals(""))
+            {
+                // Middle column won
+                //MessageBox.Show("middle column");
+                gameOver = true;
+                mvm.Winner = Convert.ToString(A2.Content);
+            }
+            else if (A3.Content.Equals(B3.Content)
+                    && A3.Content.Equals(C3.Content)
+                    && B3.Content.Equals(C3.Content)
+                    && !A3.Content.Equals(""))
+            {
+                // Right column won
+                //MessageBox.Show("right column");
+                gameOver = true;
+                mvm.Winner = Convert.ToString(A3.Content);
+            }
+
+            return gameOver;
+        }
+
+        private bool CheckDiagonal()
+        {
+            bool gameOver = false;
+            // Diagonal check
+            if (A1.Content.Equals(B2.Content)
+                && A1.Content.Equals(C3.Content)
+                && B2.Content.Equals(C3.Content)
+                && !A1.Content.Equals(""))
+            {
+                // Top left to bottom right won
+                //MessageBox.Show("tl-br");
+                gameOver = true;
+                mvm.Winner = Convert.ToString(A1.Content);
+            }
+            else if (C1.Content.Equals(B2.Content)
+                    && C1.Content.Equals(A3.Content)
+                    && B2.Content.Equals(A3.Content)
+                    && !C1.Content.Equals(""))
+            {
+                // Bottom left to top right won
+                //MessageBox.Show("bl-tr");
+                gameOver = true;
+                mvm.Winner = Convert.ToString(C1.Content);
+            }
+
+            return gameOver;
         }
     }
 }
--- Zadatak_1/MainWindowViewModel.cs
+++ Zadatak_1/MainWindowViewModel.cs
@@ -1,16 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Zadatak_1
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
-        public const string O_SYMBOL = "O";
-        public const string X_SYMBOL = "X";
+        public ObservableCollection<Button> GameButtons = new ObservableCollection<Button>();
+
+        public string X_SYMBOL = "X";
+        public string O_SYMBOL = "O";
+        public string Winner;
+
+
+        public void PerformAiMove()
+        {
+            List<Button> notClicked = new List<Button>();
+
+            foreach (Button button in GameButtons)
+            {
+                if (button.IsEnabled == true)
+                {
+                    notClicked.Add(button);
+                }
+            }
+
+            Random random = new Random();
+            int index = random.Next(0, notClicked.Count);
+            Button move = notClicked.ElementAt(index);
+
+            move.Content = O_SYMBOL;
+            move.IsEnabled = false;
+        }
+
+        public void DisableGame()
+        {
+            foreach (Button button in GameButtons)
+            {
+                button.IsEnabled = false;
+            }
+        }
+
+        public void ResetTheGame()
+        {
+            foreach (Button button in GameButtons)
+            {
+                button.IsEnabled = true;
+                button.Content = "";
+            }
+        }
+
+        public bool CheckForTie()
+        {
+            foreach (Button button in GameButtons)
+            {
+                if (button.IsEnabled == true)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
