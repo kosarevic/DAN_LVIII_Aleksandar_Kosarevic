c689024197644b63ae2a2083d72b432f324d8c60
Kosarevic <kosarevic@gmail.com>
2020-08-31 14:50:28 +0200

    Added comments to the code.

--- Zadatak_1/MainWindow.xaml.cs
+++ Zadatak_1/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Zadatak_1
         public MainWindow()
         {
             InitializeComponent();
-
+            //All buttons that represent fields are added to observable collection for purpose of certain functionalities in ViewModel class.
             mvm.GameButtons.Add(A1);
             mvm.GameButtons.Add(A2);
             mvm.GameButtons.Add(A3);
@@ -38,51 +38,64 @@ namespace Zadatak_1
 
             DataContext = mvm;
         }
-
+        /// <summary>
+        /// Method for reset button functionaliy.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnRestartButton_Click(object sender, RoutedEventArgs e)
         {
             mvm.ResetTheGame();
         }
 
+        /// <summary>
+        /// Method called when any field is pressed ether by Player or AI.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void GameAction_Click(object sender, RoutedEventArgs e)
         {
             Button pressedButton = (Button)sender;
-
+            //Player button is automaticly assinged "X" symbol.
             pressedButton.Content = mvm.X_SYMBOL;
             pressedButton.IsEnabled = false;
-
+            //Game status is checked after player has selected its move.
             if (CheckGameStatus())
                 return;
-
+            //If no wictory condition is met, AI gets a turn.
             mvm.PerformAiMove();
-
+            //After AI has played, game checks for win conditions again.
             if (CheckGameStatus())
                 return;
         }
-
+        /// <summary>
+        /// Method checks for win condition and displays which player has won the game (User or AI).
+        /// </summary>
+        /// <returns></returns>
         private bool CheckGameStatus()
         {
+            //Horisontal win conditons are checked.
             if (CheckHorizontal())
             {
                 mvm.DisableGame();
                 MessageBox.Show("Player " + mvm.Winner + " has won the game!");
                 return true;
             }
-
+            //Vertical win conditions are checked.
             if (CheckVertical())
             {
                 mvm.DisableGame();
                 MessageBox.Show("Player " + mvm.Winner + " has won the game!");
                 return true;
             }
-
+            //Diagonal win conditions are checked.
             if (CheckDiagonal())
             {
                 mvm.DisableGame();
                 MessageBox.Show("Player " + mvm.Winner + " has won the game!");
                 return true;
             }
-
+            //If no previous conditions are met, code checks for Tie conditions.
             if (mvm.CheckForTie())
             {
                 mvm.ResetTheGame();
@@ -92,38 +105,38 @@ namespace Zadatak_1
 
             return false;
         }
-
+        /// <summary>
+        /// Method for horizontal win conditions checking logic.
+        /// </summary>
+        /// <returns></returns>
         private bool CheckHorizontal()
         {
             bool gameOver = false;
-            // Horizontal check
+            //Horizontal check of A fields values.
             if (A1.Content.Equals(A2.Content)
                 && A1.Content.Equals(A3.Content)
                 && A2.Content.Equals(A3.Content)
                 && !A1.Content.Equals(""))
             {
-                // Top row won
-                //MessageBox.Show("top row");
+                //If conditions are met, method returns True and player assinged value (X or O).
                 gameOver = true;
                 mvm.Winner = Convert.ToString(A1.Content);
             }
+            //Horizontal check of B fields values.
             else if (B1.Content.Equals(B2.Content)
                     && B1.Content.Equals(B3.Content)
                     && B2.Content.Equals(B3.Content)
                     && !B1.Content.Equals(""))
             {
-                // Middle row won
-                //MessageBox.Show("middle row");
                 gameOver = true;
                 mvm.Winner = Convert.ToString(B1.Content);
             }
+            //Horizontal check of C fields values.
             else if (C1.Content.Equals(C2.Content)
                     && C1.Content.Equals(C3.Content)
                     && C2.Content.Equals(C3.Content)
                     && !C1.Content.Equals(""))
             {
-                // Bottom row won
-                //MessageBox.Show("bottom row");
                 gameOver = true;
                 mvm.Winner = Convert.ToString(C1.Content);
             }
@@ -131,17 +144,20 @@ namespace Zadatak_1
             return gameOver;
         }
 
+        /// <summary>
+        /// Method for Vertical win conditions checking logic.
+        /// </summary>
+        /// <returns></returns>
         private bool CheckVertical()
         {
             bool gameOver = false;
-            // Vertical check
+            //Vertical check of fields values.
             if (A1.Content.Equals(B1.Content)
                 && A1.Content.Equals(C1.Content)
                 && B1.Content.Equals(C1.Content)
                 && !A1.Content.Equals(""))
             {
-                // Left column won
-                //MessageBox.Show("left column");
+                //If conditions are met, method returns True and player assinged value (X or O).
                 gameOver = true;
                 mvm.Winner = Convert.ToString(A1.Content);
             }
@@ -150,8 +166,6 @@ namespace Zadatak_1
                     && B2.Content.Equals(C2.Content)
                     && !A2.Content.Equals(""))
             {
-                // Middle column won
-                //MessageBox.Show("middle column");
                 gameOver = true;
                 mvm.Winner = Convert.ToString(A2.Content);
             }
@@ -160,8 +174,6 @@ namespace Zadatak_1
                     && B3.Content.Equals(C3.Content)
                     && !A3.Content.Equals(""))
             {
-                // Right column won
-                //MessageBox.Show("right column");
                 gameOver = true;
                 mvm.Winner = Convert.ToString(A3.Content);
             }
@@ -169,17 +181,20 @@ namespace Zadatak_1
             return gameOver;
         }
 
+        /// <summary>
+        /// Method for diagonal win conditions checking logic.
+        /// </summary>
+        /// <returns></returns>
         private bool CheckDiagonal()
         {
             bool gameOver = false;
-            // Diagonal check
+            //Diagonal check of fields values.
             if (A1.Content.Equals(B2.Content)
                 && A1.Content.Equals(C3.Content)
                 && B2.Content.Equals(C3.Content)
                 && !A1.Content.Equals(""))
             {
-                // Top left to bottom right won
-                //MessageBox.Show("tl-br");
+                //If conditions are met, method returns True and player assinged value (X or O).
                 gameOver = true;
                 mvm.Winner = Convert.ToString(A1.Content);
             }
@@ -188,8 +203,6 @@ namespace Zadatak_1
                     && B2.Content.Equals(A3.Content)
                     && !C1.Content.Equals(""))
             {
-                // Bottom left to top right won
-                //MessageBox.Show("bl-tr");
                 gameOver = true;
                 mvm.Winner = Convert.ToString(C1.Content);
             }
--- Zadatak_1/MainWindowViewModel.cs
+++ Zadatak_1/MainWindowViewModel.cs
@@ -10,35 +10,46 @@ using System.Windows.Controls;
 
 namespace Zadatak_1
 {
+    /// <summary>
+    /// Class responsible for various functionalities regarding main window.
+    /// </summary>
     class MainWindowViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<Button> GameButtons = new ObservableCollection<Button>();
-
+        //Simbols for Tic Tac Toe game are predefined, meaning User gets an X and AI gets an O.
         public string X_SYMBOL = "X";
         public string O_SYMBOL = "O";
         public string Winner;
 
-
+        /// <summary>
+        /// Ai Logic for selecting fields in a game.
+        /// </summary>
         public void PerformAiMove()
         {
+            //Temporary collection for empty fields in a game.
             List<Button> notClicked = new List<Button>();
 
             foreach (Button button in GameButtons)
             {
                 if (button.IsEnabled == true)
                 {
+                    //If field was not clicked previously, it is added to temporary list.
                     notClicked.Add(button);
                 }
             }
-
+            //Next AI action is determined randomly.
             Random random = new Random();
             int index = random.Next(0, notClicked.Count);
             Button move = notClicked.ElementAt(index);
-
+            //O sign is assigned to AI for each turn he plays.
             move.Content = O_SYMBOL;
+            //Button chosen by random number is disabled after AI select it. 
             move.IsEnabled = false;
         }
 
+        /// <summary>
+        /// Method responsible for disabling all fields after win conditions are met.
+        /// </summary>
         public void DisableGame()
         {
             foreach (Button button in GameButtons)
@@ -47,6 +58,9 @@ namespace Zadatak_1
             }
         }
 
+        /// <summary>
+        /// Method resposible for clicking Reset button. (resets the game)
+        /// </summary>
         public void ResetTheGame()
         {
             foreach (Button button in GameButtons)
@@ -56,6 +70,10 @@ namespace Zadatak_1
             }
         }
 
+        /// <summary>
+        /// Method responsible if no win conditions are achived, so the game result is "Tie".
+        /// </summary>
+        /// <returns></returns>
         public bool CheckForTie()
         {
             foreach (Button button in GameButtons)
